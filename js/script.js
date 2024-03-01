aab7de7e0bfa91b3d134aa3f2e43f18d2dc41e6f
b <barsak1177@gmail.com>
2024-03-01 15:51:44 +0100

    add overflow hidden

--- js/script.js
+++ js/script.js
@@ -3,10 +3,13 @@ const nav = document.querySelector(".js-nav");
 const menu = document.querySelector(".js-menu");
 const main = document.querySelector(".js-main");
 const closed = document.querySelector(".js-closed");
+const body = document.querySelector(".js-body");
+
 
 buttons.addEventListener("click", () => {
   nav.classList.toggle("nav--visible");
   main.classList.toggle("main--blur");
   menu.classList.toggle("menu--active");
   closed.classList.toggle("closed--active");
+  body.classList.toggle("overflow-hidden");
 });
